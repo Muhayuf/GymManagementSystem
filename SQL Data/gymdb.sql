1d94ad33a9bca53e6fe44f8e3317d6f48d4e4499
Muhammad Yusuf <114671810+Muhayuf@users.noreply.github.com>
2023-04-04 21:52:16 +0700

    Add files via upload

---
+++ SQL Data/gymdb.sql
@@ -0,0 +1,273 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 03, 2023 at 06:36 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gymdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladdpackage`
+--
+
+CREATE TABLE `tbladdpackage` (
+  `id` int(11) NOT NULL,
+  `category` varchar(45) DEFAULT NULL,
+  `titlename` varchar(450) DEFAULT NULL,
+  `PackageType` varchar(45) DEFAULT NULL,
+  `PackageDuratiobn` varchar(45) DEFAULT NULL,
+  `Price` varchar(45) DEFAULT NULL,
+  `uploadphoto` varchar(450) DEFAULT NULL,
+  `Description` varchar(450) DEFAULT NULL,
+  `create_date` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbladdpackage`
+--
+
+INSERT INTO `tbladdpackage` (`id`, `category`, `titlename`, `PackageType`, `PackageDuratiobn`, `Price`, `uploadphoto`, `Description`, `create_date`) VALUES
+(7, '6', 'Fun Aerobic Training', '9', '1 Month', '200', NULL, 'perfect for those of you who want to try a new and fun sport<br>', '2023-04-03 10:45:07'),
+(8, '11', 'Beginner Cardio Training', '11', '3 Month', '350', NULL, 'perfect for beginner who want to learn more about cardio<br>', '2023-04-03 10:47:29'),
+(9, '4', 'Diet Cardio Training', '13', '6 Month', '700', '', 'for people who want to lose their weight more effectively', '2023-04-03 10:48:18'),
+(10, '5', 'Beginner Muscle Builder', '6', '1 Month', '250', NULL, 'a beginner package for build your muscle', '2023-04-03 10:50:54'),
+(11, '5', 'Intermediant Muscle Builder', '7', '1 Month', '450', NULL, 'an intermediant package for build your muscle<br>', '2023-04-03 10:51:40'),
+(12, '5', 'Master Muscle Builder', '8', '3 Month', '1000', NULL, 'an advance package for build your muscle<br>', '2023-04-03 10:52:34');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `mobile` varchar(45) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `create_date` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`id`, `name`, `email`, `mobile`, `password`, `create_date`) VALUES
+(1, 'admin', 'admin@gmail.com', '08996675832', 'd8578edf8458ce06fbc5bb76a58c5ca4', '2022-01-19 11:25:17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblbooking`
+--
+
+CREATE TABLE `tblbooking` (
+  `id` int(11) NOT NULL,
+  `package_id` varchar(45) DEFAULT NULL,
+  `userid` varchar(45) DEFAULT NULL,
+  `booking_date` timestamp NULL DEFAULT current_timestamp(),
+  `payment` varchar(45) DEFAULT NULL,
+  `paymentType` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcategory`
+--
+
+CREATE TABLE `tblcategory` (
+  `id` int(11) NOT NULL,
+  `category_name` varchar(45) DEFAULT NULL,
+  `status` varchar(45) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tblcategory`
+--
+
+INSERT INTO `tblcategory` (`id`, `category_name`, `status`) VALUES
+(4, 'Cardio', '0'),
+(5, 'Muscle Strengthening', '0'),
+(6, 'Aerobic', '0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblpackage`
+--
+
+CREATE TABLE `tblpackage` (
+  `id` int(11) NOT NULL,
+  `cate_id` varchar(45) DEFAULT NULL,
+  `PackageName` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tblpackage`
+--
+
+INSERT INTO `tblpackage` (`id`, `cate_id`, `PackageName`) VALUES
+(6, '5', 'Beginners'),
+(7, '5', 'Intermediant'),
+(8, '5', 'Advance'),
+(9, '6', 'Fun Sport'),
+(10, 'NA', 'Super Diet'),
+(11, '4', 'Fun Sport'),
+(12, 'NA', 'Super Diet'),
+(13, '4', 'Super Diet');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblpayment`
+--
+
+CREATE TABLE `tblpayment` (
+  `id` int(11) NOT NULL,
+  `bookingID` varchar(45) DEFAULT NULL,
+  `paymentType` varchar(45) DEFAULT NULL,
+  `payment` varchar(45) DEFAULT NULL,
+  `payment_date` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbluser`
+--
+
+CREATE TABLE `tbluser` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(45) DEFAULT NULL,
+  `lname` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `mobile` varchar(45) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `state` varchar(45) DEFAULT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `create_date` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbluser`
+--
+
+INSERT INTO `tbluser` (`id`, `fname`, `lname`, `email`, `mobile`, `password`, `state`, `city`, `address`, `create_date`) VALUES
+(7, 'Muhammad', 'Yusuf', 'yusuf@gmail.com', '0987654321', 'd8578edf8458ce06fbc5bb76a58c5ca4', 'indonesia', 'cikarang', NULL, '2023-04-03 04:51:12');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbladdpackage`
+--
+ALTER TABLE `tbladdpackage`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblbooking`
+--
+ALTER TABLE `tblbooking`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblcategory`
+--
+ALTER TABLE `tblcategory`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblpackage`
+--
+ALTER TABLE `tblpackage`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblpayment`
+--
+ALTER TABLE `tblpayment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbluser`
+--
+ALTER TABLE `tbluser`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbladdpackage`
+--
+ALTER TABLE `tbladdpackage`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblbooking`
+--
+ALTER TABLE `tblbooking`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `tblcategory`
+--
+ALTER TABLE `tblcategory`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tblpackage`
+--
+ALTER TABLE `tblpackage`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `tblpayment`
+--
+ALTER TABLE `tblpayment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `tbluser`
+--
+ALTER TABLE `tbluser`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
